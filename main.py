faea6eaf81e4c36dc1532c010f5733220bb90f1b
Sammi Pau <sammipau@student.ubc.ca>
2023-12-08 22:08:32 -0800

    can pick playlist to add to - limited error handling

--- main.py
+++ main.py
@@ -8,37 +8,45 @@ intents = discord.Intents.all()
 intents.typing = False
 intents.presences = False
 
-bot = commands.Bot(command_prefix='~', intents=intents)
+bot = commands.Bot(command_prefix="~", intents=intents)
 
 @bot.command()
-async def addRecent(ctx):
+async def addRecent(ctx, playlistName):
     # hard code my spotify for now, then try to add auth via discord
     # hard code which playlist for now
     manager = SpotifyManager()
     if not manager.fail:
-        count = manager.add_recent()
-        if count == 'Error':
-            await ctx.send(f'Error adding recent')
-        if count != 0:
-            await ctx.send(f'Successfully added {count} songs.')
+        count = manager.add_recent(playlistName)
+        if count == "Error":
+            await ctx.send(f"Error adding recent")
+        elif count == "PNF":
+            await ctx.send(f"Playlist does not exist.")
+        elif count != 0:
+            await ctx.send(f"Successfully added {count} songs.")
         else:
-            await ctx.send(f'No new songs detected.')
+            await ctx.send(f"No new songs detected.")
     else:
-        await ctx.send(f'Error adding recent')
+        await ctx.send(f"Error adding recent")
 
 @bot.command()
-async def removeOld(ctx):
+async def removeOld(ctx, playlistName):
     # hard code my spotify for now, then try to add auth via discord
     # hard code which playlist for now
     manager = SpotifyManager()
     if not manager.fail: 
-        count = manager.remove_old()
-        if count != 0:
-            await ctx.send(f'Successfully removed {count} songs.')
+        count = manager.remove_old(playlistName)
+        if count == "Error":
+            await ctx.send(f"Error adding recent")
+        elif count == "PNF":
+            await ctx.send(f"Playlist does not exist.")
+        elif count != 0:
+            await ctx.send(f"Successfully removed {count} songs.")
+        elif count == 0:
+            await ctx.send(f"No old songs detected.")
         else:
-            await ctx.send(f'No old songs detected.')
+            await ctx.send(f'count')
     else:
-        await ctx.send(f'Error removing old songs.')
+        await ctx.send(f"Error removing old songs.")
 
 @bot.command()
 async def list(ctx):
@@ -50,4 +58,4 @@ async def list(ctx):
     await ctx.send(response)
 
 load_dotenv()
-bot.run(os.getenv('TOKEN'))
+bot.run(os.getenv("TOKEN"))
--- spotifyManager.py
+++ spotifyManager.py
@@ -1,10 +1,11 @@
 import spotipy
+from spotipy import SpotifyException
 from spotipy.oauth2 import SpotifyOAuth
 from datetime import datetime, timedelta, date
 
 class SpotifyManager:
     playlistPerms: str = 'playlist-modify-public playlist-modify-private user-library-read'
-    playlistID: str = '4YCCVRarApNjFf8MIXpGjw' # hard coded to circlin
+    playlistID: str = '' # hard coded to circlin
     sp: spotipy.Spotify
     url: str = 'https://accounts.spotify.com/api/token'
     token: str
@@ -16,34 +17,73 @@ class SpotifyManager:
         except:
             fail = True
     
+    def add_recent(self, playlistName):
+        self.get_playlist_id(playlistName)
+        if (self.playlistID != ''):
+            likedTracks = self.get_recently_added_tracks()
+            playlistRaw= self.sp.playlist_tracks(self.playlistID)
+            playlistTracks = self.to_uris(playlistRaw)
+            tracks = list(set(likedTracks) - set(playlistTracks))
+            if (len(tracks) != 0):
+                self.sp.playlist_add_items(self.playlistID, tracks)
+            return len(tracks)
+        return 'PNF'
+    
+    def remove_old(self, playlistName):
+        self.get_playlist_id(playlistName)
+        if (self.playlistID != ''):
+            try:
+                if (self.playlistID != ''):
+                    tracks = self.get_old_tracks()
+                if (len(tracks) != 0):
+                    self.sp.playlist_remove_all_occurrences_of_items(self.playlistID, tracks)
+                    return len(tracks)
+                if (len(tracks) == 0):
+                    return 0
+            except SpotifyException as e:
+                print("error block")
+                return "Error"
+            else:
+                return "Error"
+        return 'PNF'
+        
+    # Helper functions below
+    def get_playlist_id(self, name):
+        id = ''
+        search = self.sp.search(q = name, type = 'playlist')
+        for playlist in search['playlists']['items']:
+            if (playlist['name'] == name):
+                id = playlist['uri']
+        self.playlistID = id
+    
     def get_recently_added_tracks(self):
-        # issue: limits by 20 but specifying limit breaks it
-        allTracks = self.sp.current_user_saved_tracks()
+        allTracks1 = self.sp.current_user_saved_tracks(limit = 50)
+        allTracks2 = self.sp.current_user_saved_tracks(limit = 50, offset = 50)
         # improvement - user can specify time delta
         timeFrame: date = date.today() - timedelta(days = 30)
         tracks = []
-        for item in allTracks['items']:
+        for item in allTracks1['items']:
             added: date = datetime.strptime(item['added_at'], '%Y-%m-%dT%H:%M:%SZ').date()
             if added >= timeFrame:
                 tracks.append(item['track']['uri'])
-
+        for item in allTracks2['items']:
+            added: date = datetime.strptime(item['added_at'], '%Y-%m-%dT%H:%M:%SZ').date()
+            if added >= timeFrame:
+                tracks.append(item['track']['uri'])
         return tracks
-
-    def add_recent(self):
-        likedTracks = self.get_recently_added_tracks()
-        playlistRaw= self.sp.playlist_tracks(self.playlistID)
-        playlistTracks = self.to_uris(playlistRaw)
-        tracks = list(set(likedTracks) - set(playlistTracks))
-        self.sp.playlist_add_items(self.playlistID, tracks)
-        return len(tracks)
-    
-    def remove_old(self):
-        tracks = []
-        self.sp.playlist_remove_all_occurrences_of_items(self.playlistID, tracks)
-        return 1
     
     def to_uris(self, tracks):
         ret = []
         for item in tracks['items']:
             ret.append(item['track']['uri'])
         return ret
+    
+    def get_old_tracks(self):
+        allTracks = self.sp.playlist_tracks(self.playlistID)
+        timeFrame: date = date.today() - timedelta(days = 30)
+        tracks = []
+        for item in allTracks['items']:
+            added: date = datetime.strptime(item['added_at'], '%Y-%m-%dT%H:%M:%SZ').date()
+            if added < timeFrame:
+                tracks.append(item['track']['uri'])
+        return tracks
